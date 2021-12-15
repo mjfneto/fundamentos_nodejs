6ce4f54be2e1876c52e566d7c9f4af65e253437d
mjfneto <mjfneto@gmail.com>
2021-12-15 20:37:57 -0300

    Add params and use JSON middleware

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json()); // middleware JSON
+
 /**
  * GET ‒ Buscar uma informação no servidor
  * POST ‒ Inserir uma informação no servidor
@@ -11,15 +13,29 @@ const app = express();
  * DELETE ‒ Deletar uma informação no servidor
  */
 
+/**
+ * Tipos de parâmetros
+ *
+ * Route params: identificam um recurso para edição/deleção/buscar
+ * Query params: paginação / filtro
+ * Body params: objetos (JSON); inserção/alteração
+ */
+
 app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
   return res.json(["Curso 1", "Curso 2", "Curso 3"]);
 });
 
 app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
   return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
