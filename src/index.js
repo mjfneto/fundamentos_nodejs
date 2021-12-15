9c3369bb7837f2f7014e836a8f36095a7b24c0f1
mjfneto <mjfneto@gmail.com>
2021-12-15 19:10:33 -0300

    Fix: wrong response parameter name; should be res

--- src/index.js
+++ src/index.js
@@ -16,19 +16,19 @@ app.get("/courses", (req, res) => {
 });
 
 app.post("/courses", (req, res) => {
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
-  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.patch("/courses/:id", (req, res) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
 });
 
 app.delete("/courses/:id", (req, res) => {
-  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
+  return res.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333);
