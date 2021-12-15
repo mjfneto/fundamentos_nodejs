fde79e54b79c4e0bc76e254b0c0eeaeab2e751d5
mjfneto <mjfneto@gmail.com>
2021-12-15 18:50:41 -0300

    Adiciona metodos HTTP basicos a aplicaçao

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
+const { response } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({
-    message: "Hello World Ignite ‒ Fundamentos NodeJS!",
-  });
+/**
+ * GET ‒ Buscar uma informação no servidor
+ * POST ‒ Inserir uma informação no servidor
+ * PUT ‒ Alterar uma informação no servidor
+ * PATCH ‒ Alterar uma informação específica
+ * DELETE ‒ Deletar uma informação no servidor
+ */
+
+app.get("/courses", (req, res) => {
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return response.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 app.listen(3333);
